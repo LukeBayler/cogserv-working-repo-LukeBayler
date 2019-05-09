dcae9ec687d2c9f76593e710680d5dc4f489f80b
LukeBayler <v-lubayl@microsoft.com>
2019-05-09 12:10:12 -0700

    adding categorize images

---
+++ java/computer-vision/categorize-image/src/main/java/CategorizeImage.java
@@ -0,0 +1,88 @@
+import com.microsoft.azure.cognitiveservices.vision.computervision.*;
+import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategorizeImage {
+    public static String subKey = System.getenv("AZURE_COMPUTERVISION_API_KEY");
+    public static String baseURL = System.getenv("AZURE_ENDPOINT");
+    
+    public static void main(String[] args) {
+        try {
+            CategorizeImageSample.RunSample(baseURL, subKey);
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    private static class CategorizeImageSample {
+        public static void RunSample(String url, String key) {
+        
+            ComputerVisionClient compVisClient = ComputerVisionManager.authenticate(key).withEndpoint(url);
+        
+            String imgPath = "src\\main\\resources\\analyze-image.jpg";
+            String remotePath = "https://github.com/Azure-Samples/cognitive-services-sample-data-files/raw/master/ComputerVision/Images/landmark.jpg";
+        
+            List<VisualFeatureTypes> features = new ArrayList<>();
+            features.add(VisualFeatureTypes.CATEGORIES);
+            
+            System.out.println("\nAnalyzing local image ...");
+            CategorizeImageLocal(compVisClient, imgPath, features);
+            
+            System.out.println("\nAnalyzing image from URL ...");
+            CategorizeImageFromUrl(compVisClient, remotePath, features);
+        }
+        
+        private static void CategorizeImageLocal(ComputerVisionClient client, String path, List<VisualFeatureTypes> feats) {
+            try {
+                File rawImg = new File(path);
+                byte[] imgBytes = Files.readAllBytes(rawImg.toPath());
+                
+                ImageAnalysis analysis = client.computerVision().analyzeImageInStream()
+                    .withImage(imgBytes)
+                    .withVisualFeatures(feats)
+                    .execute();
+                
+                DisplayResults(analysis, path);
+                
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        
+        private static void CategorizeImageFromUrl(ComputerVisionClient client, String path, List<VisualFeatureTypes> feats) {
+            try {
+                ImageAnalysis analysis = client.computerVision().analyzeImage()
+                    .withUrl(path)
+                    .withVisualFeatures(feats)
+                    .execute();
+                
+                DisplayResults(analysis, path);
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        
+        private static void DisplayResults(ImageAnalysis analysis, String path) {
+            DisplayImageCategoryResults(analysis);
+
+        }
+        
+        private static void DisplayImageCategoryResults(ImageAnalysis analysis) {
+            System.out.println("\nCategories: ");
+            for (Category category : analysis.categories()) {
+                System.out.printf("\'%s\' with confidence %f\n", category.name(), category.score());
+            }
+        }
+    }
+}
