cad7efb2df601f98f4ce9879b70a3d62c425955d
LukeBayler <v-lubayl@microsoft.com>
2019-05-10 14:01:40 -0700

    checking in recognize text OCR

--- samples/java/vision/computer-vision/recognize-text-ocr/src/main/java/RecognizeTextOCR.java
+++ samples/java/vision/computer-vision/recognize-text-ocr/src/main/java/RecognizeTextOCR.java
@@ -27,56 +27,69 @@ public class RecognizeTextOCR {
             ComputerVisionClient compVisClient = ComputerVisionManager.authenticate(key).withEndpoint(url);
             //System.out.println("compVisClient.endpoint(): " + compVisClient.endpoint());
             
-            String imgPath = "src\\main\\resources\\handwritten_text.jpg";
-            String remotePath = "https://github.com/Azure-Samples/cognitive-services-sample-data-files/raw/master/ComputerVision/Images/printed_text.jpg";
+            String imgPath = "src\\main\\resources\\printed_text.jpg";
+            String remotePath = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/handwritten_text.jpg";
             
-            System.out.println("\nRecognizing handwritten text with OCR on a local image ...");
+            System.out.println("\nRecognizing printed text with OCR on a local image ...");
             RecognizeTextOCRLocal(compVisClient, imgPath);
             
             System.out.println("\nRecognizing handwritten text with OCR on a remote image ...");
-            //RecognizeTextOCRFromUrl(compVisClient, remotePath);
+            RecognizeTextOCRFromUrl(compVisClient, remotePath);
         }
         
         private static void RecognizeTextOCRLocal(ComputerVisionClient client, String path) {
-            File rawImage = new File(path);
-
             try {
+                File rawImage = new File(path);
                 byte[] imageBytes = Files.readAllBytes(rawImage.toPath());
+                
                 OcrResult ocrResult = client.computerVision().recognizePrintedTextInStream()
                     .withDetectOrientation(true)
                     .withImage(imageBytes)
                     .withLanguage(OcrLanguages.EN)
-                    .execute();        
+                    .execute();
+                    
+                DisplayResults(ocrResult);
                
             } catch (Exception e) {
                 System.out.println(e.getMessage());
                 e.printStackTrace();
             }
-        
         }
-        
+
         private static void RecognizeTextOCRFromUrl(ComputerVisionClient client, String path) {
+            try {
+                OcrResult ocrResult = client.computerVision().recognizePrintedText()
+                    .withDetectOrientation(true)
+                    .withUrl(path)
+                    .withLanguage(OcrLanguages.EN)
+                    .execute();
+                    
+                DisplayResults(ocrResult);
+               
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
         }
         
         private static void DisplayResults(OcrResult ocrResult) {
-            System.out.println("Language: " + ocrResult.language());      
-            System.out.println("Orientation: " + ocrResult.orientation());      
+            System.out.println("Text: ");
+            System.out.println("Language: " + ocrResult.language());  
             System.out.println("Text angle: " + ocrResult.textAngle());      
-            System.out.println();
+            System.out.println("Orientation: " + ocrResult.orientation());      
 
+            System.out.println("Text regions: ");
             for (OcrRegion reg : ocrResult.regions()) {
-                //System.out.println("Region: " + reg);
-                //System.out.println("Bounding box: " + reg.boundingBox());
+                System.out.println("Region bounding box: " + reg.boundingBox());
                 
-                //System.out.println("Lines: ");
                 for (OcrLine line : reg.lines()) {
-                    //System.out.println(line);
+                    System.out.println("Line bounding box: " + line.boundingBox());
                     
-                    System.out.println("Words: ");
                     for (OcrWord word : line.words()) {
-                        System.out.print(word.text() + " ");
+                        System.out.println("Word bounding box: " + word.boundingBox());
+                        System.out.println("Text: " + word.text() + " ");
                     }
-                    System.out.println();                    
+
                     System.out.println();                    
                 
                 }
