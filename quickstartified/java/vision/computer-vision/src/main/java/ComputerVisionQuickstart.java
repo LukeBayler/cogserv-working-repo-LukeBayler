4af7302f1c087779c66f6599f416e18b50f7f03a
LukeBayler <v-lubayl@microsoft.com>
2019-05-22 11:52:45 -0700

    renamed file

--- quickstartified/java/vision/computer-vision/src/main/java/ComputerVisionQuickstart.java
+++ quickstartified/java/vision/computer-vision/src/main/java/ComputerVisionQuickstart.java
@@ -9,31 +9,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerVisionQuickstarts
-{   
+{
     public static void main(String[] args)
     {
         System.out.println("\nAzure Cognitive Services Computer Vision - Java Quickstart Sample");
         RunQuickstarts();
     }
-    
+
     /*  This Quickstart for the Azure Cognitive Services Computer Vision API shows how to analyze
-     ***  an image both locally (insert anchor) and from a URL (insert anchor).
-     *  
-     ***  anchor to code for analyzing both local and remote images?
+     *  an image both locally and from a URL.
      *  Analyzing an image includes:
      *  - Displaying image captions and confidence values
      *  - Displaying image category names and confidence values
      *  - Displaying image tags and confidence values
-     *  - Displaying any faces found in the image and their location
+     *  - Displaying any faces found in the image and their bounding boxes
      *  - Displaying whether any adult or racy content was detected and the confidence values
      *  - Displaying the image color scheme
-     *  - Displaying any celebrities detected in the image and their locations
-     *  - Displaying any landmarks detected in the image and their locations
+     *  - Displaying any celebrities detected in the image and their bounding boxes
+     *  - Displaying any landmarks detected in the image and their bounding boxes
      *  - Displaying what type of clip art or line drawing the image is
      *
      */
-    
-    
+
+
     public static void RunQuickstarts()
     {
         /*  Configure the local environment:
@@ -46,34 +44,34 @@ public class ComputerVisionQuickstarts
          *
          *  Note that environment variables cannot contain quotation marks, so the quotation marks
          *  are included in the code below to stringify them.
-         *  
+         *
          *  Note that after setting these environment variables in your preferred command shell,
-         *  you will need to close and then re-open your command shell.  
+         *  you will need to close and then re-open your command shell.
          */
 
         String azureComputerVisionApiKey = System.getenv("AZURE_COMPUTERVISION_API_KEY");
         String azureRegion = System.getenv("AZURE_REGION");
         //  END - Configure the local environment.
-        
-        
+
+
         /*  Create an authenticated Computer Vision client:
-         *  
+         *
          *  Concatenate the Azure region with the Azure base URL to create the endpoint URL, and
          *  then create an authenticated client with the API key and the endpoint URL.
          */
-         
+
         String endpointUrl = ("https://").concat(azureRegion).concat(".api.cognitive.microsoft.com");
         ComputerVisionClient compVisClient = ComputerVisionManager.authenticate(azureComputerVisionApiKey).withEndpoint(endpointUrl);
         //  END - Create an authenticated Computer Vision client.
-    
-    
+
+
         /*  Analyze a local image:
-         *  
-         *  Set a string variable equal to the path of a local image. The image path below is a relative path.  
+         *
+         *  Set a string variable equal to the path of a local image. The image path below is a relative path.
          */
         String pathToLocalImage = "src\\main\\resources\\landmark.jpg";
-    
-        //  This list defines the features to be extracted from the image. 
+
+        //  This list defines the features to be extracted from the image.
         List<VisualFeatureTypes> featuresToExtractFromLocalImage = new ArrayList<>();
         featuresToExtractFromLocalImage.add(VisualFeatureTypes.DESCRIPTION);
         featuresToExtractFromLocalImage.add(VisualFeatureTypes.CATEGORIES);
@@ -82,27 +80,27 @@ public class ComputerVisionQuickstarts
         featuresToExtractFromLocalImage.add(VisualFeatureTypes.ADULT);
         featuresToExtractFromLocalImage.add(VisualFeatureTypes.COLOR);
         featuresToExtractFromLocalImage.add(VisualFeatureTypes.IMAGE_TYPE);
-        
-        System.out.println("\nAnalyzing local image ...");       
-        
+
+        System.out.println("\nAnalyzing local image ...");
+
         try
         {
-            //  Need a byte array for analyzing a local image. 
+            //  Need a byte array for analyzing a local image.
             File rawImage = new File(pathToLocalImage);
             byte[] imageByteArray = Files.readAllBytes(rawImage.toPath());
-        
+
             //  Call the Computer Vision service and tell it to analyze the loaded image.
             ImageAnalysis analysis = compVisClient.computerVision().analyzeImageInStream()
                 .withImage(imageByteArray)
                 .withVisualFeatures(featuresToExtractFromLocalImage)
                 .execute();
-        
+
             //  Display image captions and confidence values.
             System.out.println("\nCaptions: ");
             for (ImageCaption caption : analysis.description().captions()) {
                 System.out.printf("\'%s\' with confidence %f\n", caption.text(), caption.confidence());
             }
-            
+
             //  Display image category names and confidence values.
             System.out.println("\nCategories: ");
             for (Category category : analysis.categories()) {
@@ -113,8 +111,8 @@ public class ComputerVisionQuickstarts
             System.out.println("\nTags: ");
             for (ImageTag tag : analysis.tags()) {
                 System.out.printf("\'%s\' with confidence %f\n", tag.name(), tag.confidence());
-            }           
-            
+            }
+
             //  Display any faces found in the image and their location.
             System.out.println("\nFaces: ");
             for (FaceDescription face : analysis.faces()) {
@@ -122,7 +120,7 @@ public class ComputerVisionQuickstarts
                     face.faceRectangle().left(), face.faceRectangle().top(),
                     face.faceRectangle().left() + face.faceRectangle().width(),
                     face.faceRectangle().top() + face.faceRectangle().height());
-            }            
+            }
 
             //  Display whether any adult or racy content was detected and the confidence values.
             System.out.println("\nAdult: ");
@@ -152,7 +150,7 @@ public class ComputerVisionQuickstarts
                     }
                 }
             }
-            
+
             //  Display any landmarks detected in the image and their locations.
             System.out.println("\nLandmarks: ");
             for (Category category : analysis.categories())
@@ -171,22 +169,22 @@ public class ComputerVisionQuickstarts
             System.out.println("Clip art type: " + analysis.imageType().clipArtType());
             System.out.println("Line drawing type: " + analysis.imageType().lineDrawingType());
         }
-        
+
         catch (Exception e)
         {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
         //  END - Analyze a local image.
-        
-        
+
+
         /*  Analyze an image from a URL:
-         *  
-         *  Set a string variable equal to the path of a remote image.  
+         *
+         *  Set a string variable equal to the path of a remote image.
          */
         String pathToRemoteImage = "https://github.com/Azure-Samples/cognitive-services-sample-data-files/raw/master/ComputerVision/Images/faces.jpg";
-    
-        //  This list defines the features to be extracted from the image. 
+
+        //  This list defines the features to be extracted from the image.
         List<VisualFeatureTypes> featuresToExtractFromRemoteImage = new ArrayList<>();
         featuresToExtractFromRemoteImage.add(VisualFeatureTypes.DESCRIPTION);
         featuresToExtractFromRemoteImage.add(VisualFeatureTypes.CATEGORIES);
@@ -195,9 +193,9 @@ public class ComputerVisionQuickstarts
         featuresToExtractFromRemoteImage.add(VisualFeatureTypes.ADULT);
         featuresToExtractFromRemoteImage.add(VisualFeatureTypes.COLOR);
         featuresToExtractFromRemoteImage.add(VisualFeatureTypes.IMAGE_TYPE);
-        
-        System.out.println("\n\nAnalyzing an image from a URL ...");       
-        
+
+        System.out.println("\n\nAnalyzing an image from a URL ...");
+
         try
         {
             //  Call the Computer Vision service and tell it to analyze the loaded image.
@@ -205,13 +203,13 @@ public class ComputerVisionQuickstarts
                 .withUrl(pathToRemoteImage)
                 .withVisualFeatures(featuresToExtractFromRemoteImage)
                 .execute();
-        
+
             //  Display image captions and confidence values.
             System.out.println("\nCaptions: ");
             for (ImageCaption caption : analysis.description().captions()) {
                 System.out.printf("\'%s\' with confidence %f\n", caption.text(), caption.confidence());
             }
-            
+
             //  Display image category names and confidence values.
             System.out.println("\nCategories: ");
             for (Category category : analysis.categories()) {
@@ -222,8 +220,8 @@ public class ComputerVisionQuickstarts
             System.out.println("\nTags: ");
             for (ImageTag tag : analysis.tags()) {
                 System.out.printf("\'%s\' with confidence %f\n", tag.name(), tag.confidence());
-            }           
-            
+            }
+
             //  Display any faces found in the image and their location.
             System.out.println("\nFaces: ");
             for (FaceDescription face : analysis.faces()) {
@@ -231,7 +229,7 @@ public class ComputerVisionQuickstarts
                     face.faceRectangle().left(), face.faceRectangle().top(),
                     face.faceRectangle().left() + face.faceRectangle().width(),
                     face.faceRectangle().top() + face.faceRectangle().height());
-            }            
+            }
 
             //  Display whether any adult or racy content was detected and the confidence values.
             System.out.println("\nAdult: ");
@@ -261,7 +259,7 @@ public class ComputerVisionQuickstarts
                     }
                 }
             }
-            
+
             //  Display any landmarks detected in the image and their locations.
             System.out.println("\nLandmarks: ");
             for (Category category : analysis.categories())
@@ -279,9 +277,9 @@ public class ComputerVisionQuickstarts
             System.out.println("\nImage type:");
             System.out.println("Clip art type: " + analysis.imageType().clipArtType());
             System.out.println("Line drawing type: " + analysis.imageType().lineDrawingType());
-            //  END - Analyze an image from a URL.            
+            //  END - Analyze an image from a URL.
         }
-        
+
         catch (Exception e)
         {
             System.out.println(e.getMessage());
