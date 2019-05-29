9e0e7fd17bf06cfab4c73a5a3f274f778bdf8f2c
LukeBayler <v-lubayl@microsoft.com>
2019-05-29 15:58:46 -0700

    checking in the day's work

---
+++ samples/java/computer-vision/alt-ComputerVisionQuickstart.java
@@ -0,0 +1,70 @@
+// import com.microsoft.azure.cognitiveservices.vision.computervision.*;
+// import com.microsoft.azure.cognitiveservices.vision.computervision.models.*;
+
+/*  Import the required libraries. If this is your first time running a Go program,
+ *  you will need to 'go get' the azure-sdk-for-go and go-autorest packages.
+ */
+
+//
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.nio.file.Files;
+//
+// import java.util.ArrayList;
+// import java.util.List;
+
+/*  The Quickstarts in this file are for the Computer Vision API for Microsoft
+ *  Cognitive Services. In this file are Quickstarts for the following tasks:
+ *  - Describing images
+ *  - Categorizing images
+ *  - Tagging images
+ *  - Detecting faces
+ *  - Detecting adult or racy content
+ *  - Detecting the color scheme
+ *  - Detecting domain-specific content (celebrities/landmarks)
+ *  - Detecting image types (clip art/line drawing)
+ *  - Detecting objects
+ *  - Detecting brands
+ *  - Recognizing printed and handwritten text with (Read API)
+ *  - Recognizing printed and handwritten text with (OCR)
+ */
+
+public class ComputerVisionQuickStarts {
+  public static void main(String[] args) {
+    // /*  Configure the local environment:
+    //  *
+    //  *  Set the AZURE_COMPUTERVISION_API_KEY and AZURE_REGION environment variables on your
+    //  *  local machine using the appropriate method for your preferred command shell.
+    //  *
+    //  *  For AZURE_REGION, use the same region you used to get your subscription keys.
+    //  ***Can we link (or just put the URL here) to the docs for finding your region?
+    //  *
+    //  *  Note that environment variables cannot contain quotation marks, so the quotation marks
+    //  *  are included in the code below to stringify them.
+    //  *
+    //  *  Note that after setting these environment variables in your preferred command shell,
+    //  *  you will need to close and then re-open your command shell.
+    //  */
+    //
+    // String azureComputerVisionApiKey = System.getenv("COMPUTERVISION_API_KEY");
+    // //  TODO - add error checking
+    //
+    // String azureRegion = System.getenv("COMPUTERVISION_REGION");
+    // //  TODO - add error checking
+    //
+    // //  END - Configure the local environment.
+    //
+    //
+    // /*  Create an authenticated Computer Vision client:
+    //  *
+    //  *  Concatenate the Azure region with the Azure base URL to create the endpoint URL, and
+    //  *  then create an authenticated client with the API key and the endpoint URL.
+    //  */
+    //
+    // String endpointUrl = ("https://").concat(azureRegion).concat(".api.cognitive.microsoft.com");
+    // ComputerVisionClient compVisClient = ComputerVisionManager.authenticate(azureComputerVisionApiKey).withEndpoint(endpointUrl);
+    // //  END - Create an authenticated Computer Vision client.
+    //
+    // System.out.println("fdjaso;");
+  }
+}
